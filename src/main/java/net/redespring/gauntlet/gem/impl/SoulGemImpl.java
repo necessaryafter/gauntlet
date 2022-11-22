1c32b32bab5d737bfea8b6ca0d0435ef2b7c5156
onlyafter <premiumbruno9@gmail.com>
2022-11-22 16:36:16 -0300

    feat: added soul gem

--- src/main/java/net/redespring/gauntlet/gem/impl/SoulGemImpl.java
+++ src/main/java/net/redespring/gauntlet/gem/impl/SoulGemImpl.java
@@ -2,6 +2,8 @@ package net.redespring.gauntlet.gem.impl;
 
 import me.lucko.helper.item.ItemStackBuilder;
 import net.redespring.gauntlet.gem.Gem;
+import org.bukkit.DyeColor;
+import org.bukkit.Material;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.potion.PotionEffect;
 
@@ -14,7 +16,11 @@ public class SoulGemImpl implements Gem {
 
     @Override
     public ItemStack getItemStack() {
-        return ItemStackBuilder.of();
+        return ItemStackBuilder.of(Material.INK_SACK)
+                .data(DyeColor.ORANGE.getData())
+                .name("§aJoia do " + getName())
+                .lore("§7Esta joia aprimora todos os seus efeitos.")
+                .build();
     }
 
     @Override
